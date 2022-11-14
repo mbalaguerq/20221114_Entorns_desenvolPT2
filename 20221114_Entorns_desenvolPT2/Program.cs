d9d3c016fdd87d3244d2e20945d21aafae2a8287
mbalaguerq <118165574+mbalaguerq@users.noreply.github.com>
2022-11-14 11:38:15 +0100

    Agregar archivos de proyecto.

---
+++ 20221114_Entorns_desenvolPT2/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20221114_Entorns_desenvolPT2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+
+        //Utilizando Visual Studio o Eclipse realiza un programa en el que incluya como mínimo un bucle,
+        //condiciones y que muestre información por pantalla.
+
+        {
+            int nValors;
+            int i = 1;
+            int numeros = 0;
+            int suma = 0;
+
+            Console.WriteLine("Bon dia, quants valors introduiràs?: ");
+            nValors = int.Parse(Console.ReadLine());
+
+            do
+            {
+                if (nValors < 0)
+                {
+                    Console.WriteLine("Que me estás contando!");
+
+                    Console.WriteLine("Quants valors introduiràs?: ");
+                    nValors = int.Parse(Console.ReadLine());
+                }
+            }
+            while (nValors < 0);
+                    
+            if (nValors > 0)
+            {
+
+                while (i <= nValors)
+                {
+                    
+
+                    Console.WriteLine("introdueix el valor N" + i);
+                    numeros = int.Parse(Console.ReadLine());
+
+                    suma += numeros;
+                    i++;
+                }
+                Console.WriteLine("La suma dels números introduits és: " + suma);
+            }
+
+        }
+    }
+}
